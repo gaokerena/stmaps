758ab77bc98be2128e8163eabd32408d2edb4a7c
gaokerena <amiel.mathieu@etik.com>
2025-09-09 18:44:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbxHz5OBOFSrpRUZlKqL_5h-yk3jVJkW9wrKd2YXUm7Of-iRzY0zitxt_LGNj7jXifAW/exec";
 
-function parseData(data) {
-       // This function is called by your JSONP response
+fetch(scriptURL)
+  .then(response => response.json())
+  .then(data => {
     const container = document.getElementById("data-container");
     container.innerHTML = ""; // Clear "Loading..."
 
     data.forEach(item => {
-      // Create a new div for each item
       const card = document.createElement("div");
-      card.className = "card"; // Optional CSS class
-
-      // Add content
+      card.className = "card";
       card.innerHTML = `
         <strong>${item.nom}</strong><br/>
         Catégorie: ${item.categorie}<br/>
         Plancher: ${item.plancher}, Plafond: ${item.plafond}<br/>
         P1: ${item.p1}
       `;
-
-      // Append the div to the container
       container.appendChild(card);
-  });
-}
+    });
+  })
+  .catch(err => console.error("Error fetching data:", err));
+
 
